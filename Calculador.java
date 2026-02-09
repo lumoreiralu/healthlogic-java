e5b4bff1c8dc4a6e61453dcd46cbe92b49ce4283
lumoreira <lulii.moreira96@gmail.com>
2026-02-09 18:25:31 -0300

    agrego TMB

--- Calculador.java
+++ Calculador.java
@@ -1,3 +1,5 @@
 public interface Calculador {
     public double calcularIMC(double peso, double altura);
+    String obtenerCategoriaIMC(double imc);
+    double calcularTMB(Paciente p, Medida m);
 }
---
+++ CalculadorSalud.java
@@ -0,0 +1,35 @@
+public class CalculadorSalud implements Calculador{
+
+    public CalculadorSalud() {
+    }
+
+    // metodo para calcular el IMC
+    public double calcularIMC(double peso, double altura) {
+        if (altura <= 0) {
+            throw new IllegalArgumentException("La altura debe ser mayor a cero");
+        }
+        return peso / (altura * altura);
+    }
+        
+    public String obtenerCategoriaIMC(double imc) {
+        if (imc < 18.5) return "Bajo peso";
+        if (imc < 24.9) return "Peso normal (Eutrófico)";
+        if (imc < 29.9) return "Sobrepeso";
+        return "Obesidad";
+    }
+
+    @Override
+    public double calcularTMB(Paciente p, Medida m) {
+            double tmb;
+        // la altura debe estar en cm para esta fórmula
+        double alturaCm = m.getAltura() * 100;
+
+        if (p.getSexo().equalsIgnoreCase("Femenino")) {
+            tmb = 655 + (9.6 * m.getPeso()) + (1.8 * alturaCm) - (4.7 * p.getEdad());
+        } else {
+            tmb = 66 + (13.7 * m.getPeso()) + (5 * alturaCm) - (6.8 * p.getEdad());
+        }
+        return tmb;
+    }
+
+}
